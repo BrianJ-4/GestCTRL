bc1d61268a8da40e41f744a740f37d381a97d9a8
BrianJ-4 <brian.joshua03@gmail.com>
2025-04-21 14:43:37 -0700

    Add pose_action_manager and update code to implement it

---
+++ pose_action_manager.py
@@ -0,0 +1,40 @@
+import json
+
+class PoseActionManager():
+    def __init__(self):
+        self.mappings = self.load_mappings()
+
+    def load_mappings(self):
+        try:
+            with open("data/mappings.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            print("Warning: mappings.json not found. Using empty mappings dictionary.")
+            return {}
+        
+    def save_mappings(self):
+        with open("data/mappings.json", "w") as file:
+            json.dump(self.mappings, file)
+    
+    def get_pose_action(self, pose):
+        return self.mappings.get(pose)
+    
+    def set_pose_action(self, pose, action):
+        self.mappings[pose] = action
+        self.save_mappings()
+
+    def delete_mapping(self, pose):
+        self.mappings[pose] = ""
+        self.save_mappings()
+
+    def delete_pose(self, pose):
+        self.mappings.pop(pose)
+        self.save_mappings()
+
+    def get_mappings(self):
+        return self.mappings
+    
+    def add_pose(self, pose):
+        self.mappings[pose] = ""
+        self.save_mappings()
+        
