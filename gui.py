4c4c9bf4b8897f3c48f9dc845d39c493e3942aec
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2025-04-25 16:47:19 -0700

    Added webcam example

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import cv2
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -12,6 +13,11 @@ class GestureApp:
         self.root.config(bg="#3b3b3b")
         self.logo = ImageTk.PhotoImage(Image.open('assets/icons/logo_nobg1.png').resize((300, 100)))
         self.start_ui()
+
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            raise RuntimeError("Webcam unable to open")
+        self.camera_preview()
 
     def set_geometry(self, parent, width, height):
         screen_width = parent.winfo_screenwidth()
@@ -38,6 +44,9 @@ class GestureApp:
         #Preview Frame (Here as an example)
         self.preview_frame = ttk.Frame(self.root, width=650, height=300)
         self.preview_frame.pack(side="right", fill="both", padx = 10, pady = 10)
+        self.preview_frame.pack_propagate(False)
+        self.preview_label = tk.Label(self.preview_frame)
+        self.preview_label.pack(fill="both", expand=True)
 
         #Add Pose Button
         self.pose_button = ttk.Button(self.pose_frame, text="Add Pose")
@@ -48,10 +57,34 @@ class GestureApp:
         self.pose_button.pack(pady=10)
 
         #Settings Button
-        self.pose_button = ttk.Button(self.pose_frame, text="Settings")
+        self.pose_button = ttk.Button(self.pose_frame, text="Settings", command=self.settings_ui)
         self.pose_button.pack(pady=10)
+
+    def settings_ui(self):
+        self.settings_window = Toplevel(self.root)
+        self.settings_window.title("Settings")
+        self.set_geometry(self.settings_window, 900, 600)
+        self.settings_window.resizable(False, False)
+        self.settings_window.config(bg="#3b3b3b")
+
+    def camera_preview(self):
+            success, image = self.cap.read()
+            if not success:
+                return
+            rgb = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            width = self.preview_frame.winfo_width()
+            height = self.preview_frame.winfo_height()
+            results = rgb.resize((width, height))
+            self.live_camera = ImageTk.PhotoImage(results)
+            self.preview_label.config(image=self.live_camera)
+            self.root.after(15, self.camera_preview)
+        
+    def close(self):
+        if self.cap.isOpened():
+            self.cap.release()
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = GestureApp(root)
     root.mainloop()
+    app.close()
