b68e57d245661be4876375526dea06a08cc9e096
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2025-04-25 23:10:16 -0700

    Added UI Updates

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ from PIL import ImageTk, Image
 import sv_ttk
 import threading
 
+from gesture_manager import GestureManager
 from model_trainer import train_model
 from pose_action_manager import PoseActionManager
 
@@ -30,7 +31,7 @@ class GestureApp:
     def start_ui(self):
         #Pose List and Thumbnail
         self.pose_frame = tk.Frame(self.root, bg="#636363", width=300, height=300)
-        self.pose_frame.pack(side="left", fill="both", padx = 5, pady = 5)
+        self.pose_frame.pack(side="right", fill="both", padx = 5, pady = 5)
         self.pose_frame.pack_propagate(False)
         cols = ("pose", "action")
         self.pose_tree = ttk.Treeview(self.pose_frame, columns= cols, show="headings")
@@ -39,21 +40,23 @@ class GestureApp:
         self.pose_tree.column("action", anchor="center", width=100)
         self.pose_tree.heading("action", text="Action")
         self.pose_tree.pack(fill="both", expand=True)
+        self.button_row_frame = tk.Frame(self.pose_frame, bg="#636363")
+        self.button_row_frame.pack(fill="both", pady=10)
 
         #Preview Frame (Here as an example)
-        self.preview_frame = ttk.Frame(self.root, width=650, height=300)
-        self.preview_frame.pack(side="right", fill="both", padx = 10, pady = 10)
+        self.preview_frame = ttk.Frame(self.root, width=650, height=450)
+        self.preview_frame.pack(side="left", anchor="n", fill="x", padx = 10, pady = 10)
         self.preview_frame.pack_propagate(False)
         self.preview_label = tk.Label(self.preview_frame)
-        self.preview_label.pack(fill="both", expand=True)
+        self.preview_label.pack(side="top", anchor="n", fill="x", expand=True)
 
         #Add Pose Button
-        self.add_pose_button = ttk.Button(self.pose_frame, text="Add Pose")
-        self.add_pose_button.pack(pady=10)
+        self.add_pose_button = ttk.Button(self.button_row_frame, text="Add Pose")
+        self.add_pose_button.pack(side="left", padx=(50, 10))
 
         #Train Button
-        self.train_button = ttk.Button(self.pose_frame, text="Train", style="Accent.TButton", command = self.train_model_clicked )
-        self.train_button.pack(pady=10)
+        self.train_button = ttk.Button(self.button_row_frame, text="Train", style="Accent.TButton", command = self.train_model_clicked )
+        self.train_button.pack(side="right", padx=(10, 60))
 
         #Settings Button
         self.settings_button = ttk.Button(self.pose_frame, text="Settings", command=self.settings_ui)
@@ -62,11 +65,11 @@ class GestureApp:
         self.updateList()
 
     def settings_ui(self):
-        self.settings_window = Toplevel(self.root)
-        self.settings_window.title("Settings")
-        self.set_geometry(self.settings_window, 900, 600)
-        self.settings_window.resizable(False, False)
-        self.settings_window.config(bg="#3b3b3b")
+            self.settings_window = Toplevel(self.root)
+            self.settings_window.title("Settings")
+            self.set_geometry(self.settings_window, 900, 600)
+            self.settings_window.resizable(False, False)
+            self.settings_window.config(bg="#3b3b3b")
 
     def train_model_clicked(self):
         def train_thread():
@@ -83,3 +86,8 @@ class GestureApp:
         mappings = self.pose_action_manager.get_mappings()
         for pose, action in mappings.items():
             self.pose_tree.insert("", "end", values = (pose, action))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GestureApp(root)
+    root.mainloop()
